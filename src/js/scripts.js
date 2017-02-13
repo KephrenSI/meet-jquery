8e941a3290233b17f260fa43d95a51e212f38851
KephrenSI <kephren.simonis@hotamil.com>
2017-02-13 10:54:07 +0100

    Exercise part two

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,6 +6,20 @@
 * started at 13/02/2017
 */
 
+const fHandleTabClick = function( oEvent ) {
+	oEvent.preventDefault();
+	const $target = oEvent.currentTarget;
+
+	if  ( $target.parentNode.classList.contains( "active" ) ) {
+		return;
+	}
+
+	document.querySelector( "ul.nav-tabs li.active" ).classList.remove( "active" );
+	$target.parentNode.classList.add( "active" );
+	document.querySelector( ".tab-content .tab-pane.active" ).classList.remove( "active" );
+	document.getElementById( $target.getAttribute( "data-tab-target" ) ).classList.add( "active" );
+};
+
 // When page loads...
 window.addEventListener( "load", function() {
 
@@ -14,6 +28,9 @@ window.addEventListener( "load", function() {
 		$elt.setAttribute( "target", "_new" );
 	} );
 
-	// 2.
+	// 2. tab mecanism
+	Array.from( document.querySelectorAll( "ul.nav.nav-tabs a" ) ).forEach( function( $elt ) {
+		$elt.addEventListener( "click", fHandleTabClick );
+	});
 
 } );
