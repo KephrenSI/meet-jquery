4233b62fe5301a6d880e08b9915ede97099fe9f7
KephrenSI <kephren.simonis@hotamil.com>
2017-02-13 12:14:27 +0100

    Exercise one and two

--- src/scripts.js
+++ src/scripts.js
@@ -7,3 +7,29 @@
 */
 
 // TODO: with jquery
+
+let = $tabs;
+
+const fChangeTab = function( oEvent ) {
+	oEvent.preventDefault();
+
+
+	$tabs.parent().filter( ".active" ).removeClass( "active" );
+	$( this ).parent().addClass( "active" );
+	$( ".tab-content .tab-pane.active" ).revomeClass( "active" );
+	$( `#${ $( this ).data( "tab-target" ) }` ).addClass( "active" );
+};
+
+// called when DOM is ready.
+$( function() {
+
+	// 1. link with rel=external
+	$( 'a[rel*="external"]' ).attr( "target", "_new" );
+
+	// 2. Tab
+	$tabs = $( "ul.nav.nav-tabs a" );
+	$tabs.on( "click", fChangeTab );
+	// ou
+	// ( $tabs = $( "ul.nav.nav-tabs a" ) ).on( "click", fChangeTab );
+
+} );
