d4637c7decb99e71ca0db0d909b5e8f6cdc7536f
KephrenSI <kephren.simonis@hotamil.com>
2017-02-13 09:27:29 +0100

    Replace grunt with gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+/* Képhren/Meet-jquery
+*
+* /gulpfile.js - Gupl tasks
+*
+* coded by Képhren SIMONIS
+* started at 13/02/2017
+*/
+
+var gulp = require ( "gulp" ),
+	babel = require ( "gulp-babel" );
+
+// --- Task for js
+gulp.task ( "js", function() {
+	gulp.src( "src/js/**/*.js" )
+		.pipe( babel() )
+		.pipe( gulp.dest( "scripts" ) );
+} );
+
+// --- Watch tasks
+gulp.task( "watch", function() {
+	gulp.watch( "src/js/**/*.js", [ "js" ] );
+} );
+
+// --- Aliases
+
+gulp.task( "default", [ "js" ] );
+gulp.task( "work", [ "default", "watch" ] );
