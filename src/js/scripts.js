5a7bf421c24368e1e39570ced42652f3dc9094c9
Képhren SIMONIS <kephren.simonis@hotmail.com>
2017-02-20 10:22:55 +0100

    Exercise four part one

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,10 +7,15 @@
 */
 
 // TODO: with jquery
+const rEmailValidation = /([\w-\.]+)@((?:[\w]+\.)+)([a-z]{2,})/i; // http://emailregex.com/ 
 
-let $trombinoFigures;
+let $trombinoFigures,
+	$commentForm,
+	$emailInput,
+	$nameInput,
+	$commentTextarea;
 
-const fChangeTab = function( oEvent ) {
+const fHandleTab = function( oEvent ) {
 	let $this = $( this );
 
 	oEvent.preventDefault();
@@ -32,11 +37,48 @@ const fHandleTrombino = function() {
 			$next = $trombinoFigures.first();
 		}
 		$next.fadeIn();
-	} );
-	
-	
+	} );	
 };
 
+const fHandleFormValidation = function( oEvent ) {
+	let bHasErrors = false,
+		sEmail,
+		sName,
+		sComment;
+	// 1. check email
+	sEmail = ( $emailInput.val() || "" ).trim();
+	if( !rEmailValidation.test(sEmail) ){
+		console.error( "Email isn't valid!" );
+		bHasErrors = true;
+	}else{
+		console.info( "Email is valid!" );
+	}
+
+	// 2. check name
+	sName = ( $nameInput.val() || "" ).trim ();
+	if( sName.length < 4 ) {
+		console.error( "Name isn't valid!" );
+		bHasErrors = true;
+	}else{
+		console.info( "Name is valid!" );
+	}
+
+	// 3. check comment
+	sComment = ( $commentTextarea.val() || "" ).trim ();
+	if( sComment.length < 10 || sComment.length > 140 ) {
+		console.error( "Comment isn't valid!" );
+		bHasErrors = true;
+	}else{
+		console.info( "Comment is valid!" );
+	}
+
+	if( bHasErrors ) {
+		window.alert( "Veuillez remplir tous les champs de formulaire!" )
+		return false; // évènement ne continue pas ( form non validé )
+	}
+
+	return true; 
+};
 // called when DOM is ready.
 $( function() {
 
@@ -44,12 +86,19 @@ $( function() {
 	$( 'a[rel*="external"]' ).attr( "target", "_new" );
 
 	// 2. Tab
-	$( "ul.nav.nav-tabs a" ).on( "click", fChangeTab );
-	
+	$( "ul.nav.nav-tabs a" ).on( "click", fHandleTab );
+
 	// 3. trombinoscope
 	$trombinoFigures = $( "#trombino figure" );
 	$trombinoFigures.hide().first().show();
 	setInterval( fHandleTrombino, 1000 );
+
+	// 4. handle form validation
+	$commentForm = $( "form" );
+	$emailInput = $( "#inputEmail" );
+	$nameInput = $( "#inputName" );
+	$commentTextarea = $( "#inputComment" );
+	$commentForm.on( "submit", fHandleFormValidation );
 
 
 } );
